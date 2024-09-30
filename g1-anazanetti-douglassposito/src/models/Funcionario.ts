cd6cd51f8e2fd3686c91059974359af52f18f0b4
Ana Zanetti <moraiszanetti@gmail.com>
2024-09-30 20:05:26 -0300

    Commit Classe Funcionario

---
+++ g1-anazanetti-douglassposito/src/models/Funcionario.ts
@@ -0,0 +1,27 @@
+class Funcionario{
+            nome: string;
+            tempoDeEmpresa: number;
+            cargo: string;
+            salario: number;
+
+        constructor(nome:string, tempoDeEmpresa: number, cargo:string, salario: number){
+            this.nome= nome;
+            this.tempoDeEmpresa=tempoDeEmpresa;
+            this.cargo=cargo;
+            this.salario=salario;
+        }
+
+
+    calculaBonus():number{
+        
+        if(this.cargo="Funcionario"){
+            return Number(this.salario*0.1);
+        }     
+        else if(this.cargo="Gerente"){
+            return Number(this.salario*0.2);
+        }
+        else{
+            return Number(this.salario*0.3);
+        }
+    }
+}
